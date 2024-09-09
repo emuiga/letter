523828b18731532a1135322d7ab04775ac32c12c
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-09-09 10:36:26 +0300

    add music

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import useSound from 'use-sound';
+import { FaPlay, FaPause } from 'react-icons/fa'; // Example icons
+
 export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [play, { stop }] = useSound('sound.mp3', { loop: true,interrupt: false });
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      stop(); 
+    } else {
+      play(); 
+    }
+    setIsPlaying(!isPlaying); 
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex justify-center">
+      <button className="text-3xl p-4" onClick={togglePlay}>
+        {isPlaying ? <FaPause /> : <FaPlay />}
+      </button>
+    </div>
+  );
 }
